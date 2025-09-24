578f7c93abdeb1cd567dc4db3f9a0466723bdadd
Charlie Davidson <charliedavidson658@gmail.com>
2025-09-23 22:37:03 -0400

    Create Customer page

--- README.md
+++ README.md
@@ -4,13 +4,14 @@ This is a program developed for use in a water-restoration setting (or any other
 
 Here is an early build of the project with example data (UI is subject to change):
 ![example1.png](example1.png)
+![example2.png](example2.png)
 
 The program reads from a file called `servicelog.json` in `src/main/resources/data`, and uses it to populate the data on screen. To get started, use the file `servicelog.json.example` in the resources directory, just remove the `.example`
 
 ## Roadmap
 - [X] Initialize Project
 - [X] Create page for all active Jobs/Customers
-- [ ] Create page for each Customer
+- [X] Create page for each Customer
 - [ ] Allow users to edit Customer information
 - [ ] Allow users to edit/add new services for a Customer
 - [ ] Connect data to FireStoreDB to synchronize data across instances
--- src/main/java/org/example/jobtrackerspring/controller/ViewController.java
+++ src/main/java/org/example/jobtrackerspring/controller/ViewController.java
@@ -1,9 +1,13 @@
 package org.example.jobtrackerspring.controller;
 
+import org.example.jobtrackerspring.model.Job;
 import org.example.jobtrackerspring.repository.JobRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Controller
 public class ViewController {
@@ -18,5 +22,16 @@ public class ViewController {
     public String viewJobs(Model model) {
         model.addAttribute("jobs", jobRepo.findAll());
         return "jobs"; // resolves to templates/jobs.html
+    }
+
+    @GetMapping("/jobs/{id}")
+    public String viewJobDetail(@PathVariable Long id, Model model) {
+        Optional<Job> jobOpt = jobRepo.findById(id);
+        if (jobOpt.isEmpty()) {
+            return "redirect:/view/jobs"; // or a 404 page
+        }
+
+        model.addAttribute("job", jobOpt.get());
+        return "job-detail"; // maps to job-detail.html
     }
 }
